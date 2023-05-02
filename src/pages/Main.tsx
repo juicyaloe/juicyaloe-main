344787ec407180d87a836318a57ea1001fec2803
Min-jae Kim <mink4661@snu.ac.kr>
2023-05-02 22:34:47 +0900

    feat: add Lottie animation

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import Lottie from 'lottie-react';
+
+import GressAnimation from '../components/lottie/grass.json';
+
+import styled from '@emotion/styled';
 
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <>
+      <FixedLottie animationData={GressAnimation} />
+    </>
+  );
 }
+
+const FixedLottie = styled(Lottie)`
+  position: fixed;
+  bottom: -5px;
+
+  z-index: -1;
+`;
